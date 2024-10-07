85eec05b6fbd8a228b15a8956916c9b06f414920
Agustinrach <74275614+Agustinrach@users.noreply.github.com>
2024-10-06 21:37:16 -0300

    Update ClienteServicioImpl.java

--- src/main/java/com/example/SentirseBien/servicio/ClienteServicioImpl.java
+++ src/main/java/com/example/SentirseBien/servicio/ClienteServicioImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Service
 public class ClienteServicioImpl implements ClienteServicio{
     @Autowired
+    @Lazy
     private PasswordEncoder passwordEncoder;
 
     @Autowired
